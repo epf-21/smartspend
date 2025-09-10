15cedb607e6fad800e134cc325e4c65816091f95
epf-21 <efrainperalta.f@gmail.com>
2025-09-09 22:20:35 -0400

    feat: configure cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from app.api import categories, budgets, transactions
@@ -15,6 +16,18 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(categories.router, prefix="/api")
 app.include_router(budgets.router, prefix="/api")
