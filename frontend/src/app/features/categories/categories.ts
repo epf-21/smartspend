300ce4b68fbc414cca17f3c86f31081488b81eab
epf-21 <efrainperalta.f@gmail.com>
2025-09-21 18:40:36 -0400

    feat: Add a modal to create and update the category

--- frontend/src/app/features/categories/categories.ts
+++ frontend/src/app/features/categories/categories.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { LucideAngularModule, Pencil, Plus, Tag, Trash } from 'lucide-angular';
 import { CategoryServices } from '../../core/services/category';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -18,5 +18,40 @@ export class CategoriesComponent {
   readonly Trash = Trash;
   private categoryServices = inject(CategoryServices);
 
+  isModalOpen = signal<boolean>(false);
+  selectedCategory = signal<Category | null>(null);
+
   categories = toSignal(this.categoryServices.getCategories(), { initialValue: [] as Category[] });
+
+  openCreateModal() {
+    this.selectedCategory.set(null);
+    this.isModalOpen.set(true);
+  }
+
+  openEditModal(category: Category) {
+    this.selectedCategory.set(category);
+    this.isModalOpen.set(true);
+  }
+
+  closeModal() {
+    this.isModalOpen.set(false);
+    this.selectedCategory.set(null);
+  }
+
+  onCategorySaved(savedCategory: Category) {
+    this.categoryServices.getCategories().subscribe((categories) => {});
+  }
+
+  deleteCategory(category: Category) {
+    if (confirm(`¿Estás seguro de que quieres eliminar la categoria "${category.name}"?`)) {
+      this.categoryServices.deleteCategory(category.id).subscribe({
+        next: () => {
+          this.categoryServices.getCategories().subscribe();
+        },
+        error: (error) => {
+          console.error('Error deleting cateogory: ', error);
+        },
+      });
+    }
+  }
 }
--- frontend/src/app/features/categories/modal/modal.ts
+++ frontend/src/app/features/categories/modal/modal.ts
@@ -1,8 +1,116 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, input, output, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LucideAngularModule, Palette, Save, X } from 'lucide-angular';
+import { Category, CategoryCreate, CategoryUpdate } from '../../../core/models';
+import { CategoryServices } from '../../../core/services/category';
 
 @Component({
   selector: 'app-modal',
-  imports: [],
+  imports: [ReactiveFormsModule, LucideAngularModule],
   templateUrl: './modal.html',
 })
-export class Modal {}
+export class Modal {
+  readonly X = X;
+  readonly Save = Save;
+  readonly Palette = Palette;
+
+  isOpen = input<boolean>(false);
+  category = input<Category | null>(null);
+  onClose = output<void>();
+  onSave = output<Category>();
+
+  private fb = inject(FormBuilder);
+  private categoryServices = inject(CategoryServices);
+
+  isLoading = signal(false);
+  isEditMode = signal(false);
+
+  categoryForm: FormGroup;
+
+  predefinedColors = [
+    '#6366f1',
+    '#8b5cf6',
+    '#ec4899',
+    '#ef4444',
+    '#f59e0b',
+    '#10b981',
+    '#06b6d4',
+    '#84cc16',
+    '#f97316',
+    '#6366f1',
+    '#64748b',
+    '#374151',
+  ];
+
+  constructor() {
+    this.categoryForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(1)]],
+      color: ['#6366f1', [Validators.required]],
+      icon: ['', [Validators.required]],
+    });
+
+    effect(() => {
+      const categoryData = this.category();
+      if (categoryData) {
+        this.isEditMode.set(true);
+        this.categoryForm.patchValue({
+          name: categoryData.name,
+          color: categoryData.color,
+          icon: categoryData.icon,
+        });
+      } else {
+        this.isEditMode.set(false);
+        this.categoryForm.reset({
+          name: '',
+          color: '#6366f1',
+          icon: '',
+        });
+      }
+    });
+  }
+
+  selectColor(color: string) {
+    this.categoryForm.patchValue({ color });
+  }
+
+  closeModal() {
+    this.categoryForm.reset({
+      name: '',
+      color: '#6366f1',
+      icon: '',
+    });
+
+    this.onClose.emit();
+  }
+
+  onSubmit() {
+    if (this.categoryForm.valid && !this.isLoading()) {
+      this.isLoading.set(true);
+
+      const formValue = this.categoryForm.value;
+
+      const categoryData = {
+        name: formValue.name.trim(),
+        color: formValue.color,
+        icon: formValue.icon.trim(),
+      };
+
+      const request = this.isEditMode()
+        ? this.categoryServices.updateCategory(this.category()!.id, categoryData as CategoryUpdate)
+        : this.categoryServices.createCategory(categoryData as CategoryCreate);
+
+      console.log('request object::::::', request);
+      request.subscribe({
+        next: (savedCategory) => {
+          this.isLoading.set(false);
+          this.onSave.emit(savedCategory);
+          this.closeModal();
+        },
+        error: (error) => {
+          this.isLoading.set(false);
+          console.error('Error saving category: ', error);
+        },
+      });
+    }
+  }
+}
